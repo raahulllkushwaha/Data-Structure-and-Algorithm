6a00abe2f48f4f7af4dbc2f37ff4a3dd86e7deb6
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-06-19 07:59:23 +0530

    Write code to find total Pairs

--- src/Array/PairsInAnArray.java
+++ src/Array/PairsInAnArray.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class PairsInAnArray {
     public static void findPairs(int[] arr) {
+        int totalPairs = 0;
         for (int i = 0; i < arr.length; i++) {
             int currentElement = arr[i];
             for (int j = i + 1; j < arr.length; j++) {
                 System.out.print("(" + currentElement + ", " + arr[j] + ")" + " ");
+                totalPairs++;
             }
             System.out.println();
         }
+        System.out.println("Total Pairs: " + totalPairs);
     }
 
     public static void main(String[] args) {
