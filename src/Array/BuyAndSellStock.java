fca3932a863ee4829d014eb1c3b97a9f3d306f44
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-06-26 08:45:47 +0530

    Buy and Sell Stocks Program

--- src/Array/BuyAndSellStock.java
+++ src/Array/BuyAndSellStock.java
@@ -1,4 +1,23 @@
 package Array;
 
 public class BuyAndSellStock {
-}
+    public static int buySellStock(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) { // its a profit condition
+                int profit = prices[i] - buyPrice; // todays profit
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+            return maxProfit;
+    }
+        public static void main (String[]args){
+            int[] arr = {7, 1, 5, 3, 6, 4};
+            System.out.println(buySellStock(arr));
+        }
+    }
+
