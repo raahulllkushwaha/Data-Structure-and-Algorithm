9d1b3740d30c44c153427b11a6adadb022ab6cea
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-07-17 07:18:42 +0530

    Count number of swaps

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class BubbleSort {
     public static void bubbleSort(int arr[]) {
+        int swap = 0;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -9,9 +10,11 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swap++;
                 }
             }
         }
+        System.out.println("Number of swaps: "+ swap);
     }
 
     public static void printArray(int arr[]) {
