9b23b40e3f707c6cec52fb4de0bba89f1d775ebe
Rahul Kushwaha <raahullkushwaha@gmail.com>
2025-08-02 08:41:09 +0530

    Basic Program of 2D Array

---
+++ src/Array/TwoDArray/Creation.java
@@ -0,0 +1,22 @@
+package Array.TwoDArray;
+import java.util.Scanner;
+
+public class Creation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[4][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
